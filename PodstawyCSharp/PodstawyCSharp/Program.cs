9bd8d56498f68b8e8f9d53c4ffebfdb8863e026d
Mariusz Murawski <mariusz.murawski84@gmail.com>
2017-09-03 13:44:26 +0200

    zadanko 2

--- PodstawyCSharp/PodstawyCSharp/Program.cs
+++ PodstawyCSharp/PodstawyCSharp/Program.cs
@@ -10,6 +10,19 @@ namespace PodstawyCSharp
     {
         static void Main(string[] args)
         {
+            int min, max;
+            Console.WriteLine("Będziemy sumować dwie losowe liczby.");
+            Console.WriteLine("Podaj początek zakresu (minimum)");
+            min = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj koniec zakresu (maksimum)");
+            max = Convert.ToInt32(Console.ReadLine());
+            Random rand = new Random();
+            int first, second;
+            first = rand.Next(min, max);
+            second = rand.Next(min, max);
+            Console.WriteLine("Wylosowałem " + first + " i " + second);
+            Console.WriteLine("Suma dwóch losowych liczb z zakresu " + min + " - " + max + " jest równa " + (first + second));
+            Console.ReadKey();
         }
     }
 }
