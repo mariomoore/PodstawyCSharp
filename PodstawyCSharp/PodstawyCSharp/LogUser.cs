bc53edf7db1adb15b861d98f93bceca0f0c61abd
Mariusz Murawski <mariusz.murawski84@gmail.com>
2017-09-03 16:35:40 +0200

    Zadanko 3 - Logowanie

---
+++ PodstawyCSharp/PodstawyCSharp/LogUser.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodstawyCSharp
+{
+    class LogUser
+    {
+        private string usr;
+        private string psd;
+        private int mistakes = 0;
+
+        public LogUser()
+        {
+            usr = "Mariusz";
+            psd = "xyz";
+        }
+
+        public LogUser(string usr, string psd)
+        {
+            this.usr = usr;
+            this.psd = psd;
+            Console.WriteLine("Utworzono użytkownika " + this.usr);
+        }
+
+        public bool Logowanie()
+        {
+            while (!askForLogin())
+            {
+                Console.WriteLine("Nie ma takigo użytkownika");
+            }
+
+            while ((!askForPassword()) && mistakes < 4)
+            {
+                Console.WriteLine("Złe hasło");
+                mistakes++;
+            }
+
+            if (mistakes >= 4)
+            {
+                Console.WriteLine("Za dużo prób logowania");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Próby błędnego logowania: " + mistakes);
+                return true;
+            }
+        }
+
+        private bool askForLogin()
+        {
+            string login;
+            Console.WriteLine("Podaj login: ");
+            login = Console.ReadLine();
+            if (login == usr) return true;
+            else return false;
+        }
+
+        private bool askForPassword()
+        {
+            string pass;
+            Console.WriteLine("Podaj hasło: ");
+            pass = Console.ReadLine();
+            if (pass == psd) return true;
+            else return false;
+        }
+    }
+}
--- PodstawyCSharp/PodstawyCSharp/Program.cs
+++ PodstawyCSharp/PodstawyCSharp/Program.cs
@@ -10,6 +10,33 @@ namespace PodstawyCSharp
     {
         static void Main(string[] args)
         {
+            //Napisać program ktory sprawdza login i haslo uzytkownika do 5 nieudanych prob.
+            LogUser user = new LogUser();
+            if(user.Logowanie())
+            {
+                Console.WriteLine("Zalogowano");
+            }
+            else
+            {
+                Console.WriteLine("Nie udało się zalogować");
+            }
+            Console.WriteLine("Wciśnij klawisz");
+            Console.ReadKey();
+
+            Console.Clear();
+
+            Console.WriteLine("Nowy użytkownik");
+            LogUser otherUser = new LogUser("User", "Password");
+            if (otherUser.Logowanie())
+            {
+                Console.WriteLine("Zalogowano");
+            }
+            else
+            {
+                Console.WriteLine("Nie udało się zalogować");
+            }
+            Console.WriteLine("Wciśnij klawisz");
+            Console.ReadKey();
         }
     }
 }
