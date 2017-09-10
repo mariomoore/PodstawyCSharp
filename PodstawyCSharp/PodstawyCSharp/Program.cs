b39d100c2295e9174c1be58b739a893b920cbd01
Mariusz Murawski <mariusz.murawski84@gmail.com>
2017-09-10 23:21:36 +0200

    Zadanie: dziedziczenie po klasie abstrakcyjnej

--- PodstawyCSharp/PodstawyCSharp/Program.cs
+++ PodstawyCSharp/PodstawyCSharp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace PodstawyCSharp
 {
@@ -10,6 +11,25 @@ namespace PodstawyCSharp
     {
         static void Main(string[] args)
         {
+            Osobowy osobowy = new Osobowy();
+            Sportowy sportowy = new Sportowy();
+
+            ArrayList samochody = new ArrayList();
+
+            samochody.Add(osobowy);
+            samochody.Add(sportowy);
+
+            foreach (Samochod auto in samochody)
+            {
+                auto.WypiszMarke();
+                Console.Write(" ");
+                auto.WypiszKolor();
+                Console.Write(" ");
+                auto.WypiszPojemnosc();
+                Console.Write("\n\r");
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ PodstawyCSharp/PodstawyCSharp/Samochod.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodstawyCSharp
+{
+    public abstract class Samochod
+    {
+        protected string kolor, marka, pojemnoscSilnika;
+        public abstract void WypiszKolor();
+        public abstract void WypiszMarke();
+        public abstract void WypiszPojemnosc();
+    }
+
+    public class Osobowy : Samochod
+    {
+        public Osobowy()
+        {
+            kolor = "szary";
+            marka = "opel";
+            pojemnoscSilnika = "1.2l";
+        }
+
+        public override void WypiszKolor()
+        {
+            Console.Write("kolor " + kolor);
+        }
+
+        public override void WypiszMarke()
+        {
+            Console.Write("Zwykły " + marka);
+        }
+
+        public override void WypiszPojemnosc()
+        {
+            Console.Write("pojemnosc tylko " + pojemnoscSilnika);
+        }
+    }
+
+    public class Sportowy : Samochod
+    {
+        public Sportowy()
+        {
+            kolor = "niebieski";
+            marka = "subaru";
+            pojemnoscSilnika = "2l";
+        }
+
+        public override void WypiszKolor()
+        {
+            Console.Write("kolor " + kolor);
+        }
+
+        public override void WypiszMarke()
+        {
+            Console.Write("Sportowy " + marka);
+        }
+
+        public override void WypiszPojemnosc()
+        {
+            Console.Write("spora pojemnosc " + pojemnoscSilnika);
+        }
+    }
+}
